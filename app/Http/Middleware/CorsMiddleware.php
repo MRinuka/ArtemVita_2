72202ecde5beedaffc541e190d8a0e6e6157c6cf
MRinuka <cb010988@students.apiit.lk>
2025-02-04 12:22:27 +0530

    corsupdate

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -7,12 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization');
-    }
+        $response = $next($request);
 
+        if ($response instanceof Response) {
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
+        return $response;
+    }
 }
